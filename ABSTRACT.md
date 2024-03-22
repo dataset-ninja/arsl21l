6959f84c7238dba0de193c3059c4cb31366b4d51
peixed351 <peixed351@gmail.com>
2024-03-22 12:24:18 +0300

    add settings

--- ABSTRACT.md
+++ ABSTRACT.md
@@ -1,5 +1,10 @@
+The authors present their collected and annotated **ArSL21L: Arabic Sign Language Letter Dataset** consisting of 14202 images of 32 letter signs with various backgrounds collected from 50 people. They benchmarked the ArSL21L dataset on state-of-the-art object detection models, i.e., 4 versions of YOLOv5.
 
-Sample image template:
-<img src="https://github.com/dataset-ninja/gland-segmentation/assets/78355358/f158d0dd-71d5-41a2-aba5-4a5f57d54c35" alt="image" width="800">
+## Motivation
 
-<span style="font-size: smaller; font-style: italic;">Image description.</span>
+For individuals with hearing loss (PwHL), establishing social connections with the mainstream population can be challenging. This often necessitates interactive computer systems capable of understanding sign language. With the growing popularity of Metaverse applications employing augmented reality (AR) and virtual reality (VR), there's an opportunity to facilitate remote sign language instruction through avatars that mimic gestures, powered by AI (Artificial Intelligence) systems. While numerous methods and datasets exist for English sign language, resources for Arabic sign language are limited. Thus, the authors introduce the Arabic Sign Language Letters Dataset (ArSL21L), comprising 14,202 images depicting 32 letter signs against various backgrounds. These images were collected from 50 individuals and annotated for clarity and consistency.
+
+<img src="https://github.com/dataset-ninja/arsl21l/assets/120389559/aa16afc6-aa46-4f74-b4ba-f9a93c624190" alt="image" width="600">
+
+<span style="font-size: smaller; font-style: italic;">Arabic sign language.</span>
+
--- src/convert.py
+++ src/convert.py
@@ -2,6 +2,8 @@ import os
 import shutil
 
 import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.imaging.color import get_predefined_colors
 from supervisely.io.fs import (
     file_exists,
     get_file_name,
@@ -11,7 +13,6 @@ from supervisely.io.fs import (
 from tqdm import tqdm
 
 import src.settings as s
-from dataset_tools.convert import unpack_if_archive
 
 
 def convert_and_upload_supervisely_project(
@@ -19,72 +20,118 @@ def convert_and_upload_supervisely_project(
 ) -> sly.ProjectInfo:
     # Possible structure for bbox case. Feel free to modify as you needs.
 
-    root_path = ""
-    images_folder = "images"
-    bboxes_folder = "labels"
+    dataset_path = "/home/alex/DATASETS/TODO/ArSL21L"
+    train_path = "/home/alex/DATASETS/TODO/ArSL21L/data/train.txt"
+    val_path = "/home/alex/DATASETS/TODO/ArSL21L/data/val.txt"
     batch_size = 30
-    img_ext = ".png"
+    images_ext = ".jpg"
     ann_ext = ".txt"
 
     def create_ann(image_path):
-        labels, img_tags, label_tags = [], [], []
+        labels = []
 
-        image_np = sly.imaging.image.read(image_path)[:, :, 0]
-        img_height = image_np.shape[0]
-        img_width = image_np.shape[1]
+        # image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = 416  # image_np.shape[0]
+        img_wight = 416  # image_np.shape[1]
 
-        file_name = get_file_name(image_path)
-        curr_anns_dirpath = ""
-        ann_path = os.path.join(curr_anns_dirpath, file_name + ann_ext)
+        ann_path = image_path.replace("images", "labels").replace(images_ext, ann_ext)
 
         if file_exists(ann_path):
             with open(ann_path) as f:
                 content = f.read().split("\n")
+
                 for curr_data in content:
                     if len(curr_data) != 0:
                         curr_data = list(map(float, curr_data.split(" ")))
+                        obj_class = idx_to_class[curr_data[0]]
 
-                        left = int((curr_data[1] - curr_data[3] / 2) * img_width)
-                        right = int((curr_data[1] + curr_data[3] / 2) * img_width)
+                        left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
+                        right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
                         top = int((curr_data[2] - curr_data[4] / 2) * img_height)
                         bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
-
                         rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
-
-                        for obj_class in obj_classes:
-                            if obj_class.name == idx2clsname[curr_data[0]]:
-                                curr_obj_class = obj_class
-                                break
-                        label = sly.Label(rectangle, curr_obj_class, label_tags)
+                        label = sly.Label(rectangle, obj_class)
                         labels.append(label)
 
-        return sly.Annotation(img_size=(img_height, img_width), labels=labels, img_tags=img_tags)
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
 
-    class_names = ["class1", "class2", ...]
-    idx2clsname = {}
-    obj_classes = [sly.ObjClass(name, sly.Rectangle) for name in class_names]
+    classes_names = [
+        "ain",
+        "al",
+        "aleff",
+        "bb",
+        "dal",
+        "dha",
+        "dhad",
+        "fa",
+        "gaaf",
+        "ghain",
+        "ha",
+        "haa",
+        "jeem",
+        "kaaf",
+        "khaa",
+        "la",
+        "laam",
+        "meem",
+        "nun",
+        "ra",
+        "saad",
+        "seen",
+        "sheen",
+        "ta",
+        "taa",
+        "thaa",
+        "thal",
+        "toot",
+        "waw",
+        "ya",
+        "yaa",
+        "zay",
+    ]
+
+    idx_to_class = {}
+
+    obj_classes = [
+        sly.ObjClass(name, sly.Rectangle, color)
+        for name, color in zip(classes_names, get_predefined_colors(len(classes_names)))
+    ]
+
+    for idx, obj_class in enumerate(obj_classes):
+        idx_to_class[idx] = obj_class
 
     project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
-    meta = sly.ProjectMeta(obj_classes=obj_classes)
+    meta = sly.ProjectMeta(obj_classes=list(idx_to_class.values()))
     api.project.update_meta(project.id, meta.to_json())
 
-    for ds_name in os.listdir(root_path):
+    for split_path in [train_path, val_path]:
+
+        ds_name = get_file_name(split_path)
+
         dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
-        dataset_path = os.path.join(root_path, ds_name)
 
-        images_pathes = sly.fs.list_files_recursively(dataset_path, valid_extensions=[img_ext])
+        with open(split_path) as f:
+            content = f.read().split("\n")
 
-        pbar = tqdm(desc=f"Create dataset '{ds_name}'", total=len(images_pathes))
+        images_pathes = [
+            os.path.join(dataset_path, im_subpath) for im_subpath in content if len(im_subpath) > 1
+        ]
+
+        images_pathes = [im_path for im_path in images_pathes if file_exists(im_path)]
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
         for images_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
             images_names_batch = [
                 get_file_name_with_ext(image_path) for image_path in images_pathes_batch
             ]
 
             img_infos = api.image.upload_paths(dataset.id, images_names_batch, images_pathes_batch)
-            img_ids = [image.id for image in img_infos]
+            img_ids = [im_info.id for im_info in img_infos]
 
             anns = [create_ann(image_path) for image_path in images_pathes_batch]
             api.annotation.upload_anns(img_ids, anns)
 
-            pbar.update(len(images_names_batch))
+            progress.iters_done_report(len(images_names_batch))
+
     return project
